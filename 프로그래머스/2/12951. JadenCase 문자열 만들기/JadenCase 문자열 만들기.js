6c8b8437e0ae892a600a8a28d649129d2a37436b
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-03-14 19:26:42 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,30 +1,13 @@
 function solution(s) {
-    var answer = '';
-    var answer2='';
-    var arr=s.split(" ");
-    var arr2=[];
-    for(i=0;i<arr.length;i++){
-        let firstchar=arr[i].charAt(0);
-        if(firstchar!=firstchar.toUpperCase()){
-            firstchar=firstchar.toUpperCase();
-        }
-        arr2.push(firstchar);
-        for(let x of arr[i]){
-            if(x===x.toUpperCase()){
-                answer +=x.toLowerCase();
-            }
-            else{
-                answer+=x;
-            }
-        }
-        answer+=" ";
-    }
-    arr=answer.split(" ");
-    for(i=0;i<arr.length-1;i++){
-        arr[i]=arr[i].slice(1);
-        arr[i]=arr2[i]+arr[i];
-        answer2+=arr[i]+" ";
-    }
-    answer2=answer2.slice(0,-1);
-    return answer2;
+  let arr = s.split(' ');
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] === '' ? answer.push(arr[i]) : 
+      answer.push(
+        isNaN(arr[i][0])
+          ? arr[i][0].toUpperCase() + arr[i].slice(1).toLowerCase()
+          : arr[i][0] + arr[i].slice(1).toLowerCase()
+      );
+  }
+  return answer.join(' ');
 }
