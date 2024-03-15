2542909d652d1d0ac578f14dcfaa49ff5d269f51
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-03-15 11:40:50 +0900

    [level 0] Title: 수 조작하기 2, Time: 7.41 ms, Memory: 41.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,21 @@
+function solution(numLog) {
+    var answer = '';
+    for(let i=0;i<numLog.length;i++){
+        switch(numLog[i+1]-numLog[i]){
+            case 1 :
+               answer+='w';
+                break;
+            case -1:
+               answer+="s";
+                  break;
+            case 10:
+               answer+="d";
+                  break;
+            case -10:
+               answer+="a";
+                  break;
+                
+        }
+    }
+    return answer;
+}
