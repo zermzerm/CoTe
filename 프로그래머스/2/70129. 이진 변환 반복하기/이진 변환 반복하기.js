08550e1da6c0f21a5bb8e3dceb6bae3da84907ff
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-03-18 15:29:29 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 14.90 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -2,13 +2,9 @@ function solution(s) {
   let cnt = 0;
   let zeroCnt = 0;
   while (!(s === "1")) {
-    while (s.includes("0")) {
-      s = s.replace("0", "");
-      zeroCnt++;
-    }
-    s = s.length.toString(2);
+    if (s.match(/0/g)) zeroCnt += s.match(/0/g).length;
+    s = s.replace(/0/g, "").length.toString(2);
     cnt++;
   }
-
   return [cnt, zeroCnt];
 }
