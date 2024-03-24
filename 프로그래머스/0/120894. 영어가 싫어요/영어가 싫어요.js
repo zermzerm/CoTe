e3db88f56316b5b545c80105736aa9657fbffb8c
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-03-24 14:47:45 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,25 @@
+const eng = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+  zero: "0",
+};
+
+const solution = (numbers) => {
+  let answer = "";
+  let letter = "";
+  for (let i = 0; i < numbers.length; i++) {
+    letter += numbers[i];
+    if (eng[letter]) {
+      answer += eng[letter];
+      letter = "";
+    }
+  }
+  return +answer;
+};
