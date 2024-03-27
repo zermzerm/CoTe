d5b497620f8ad88932ff1182f25c16c3321b459c
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-03-27 10:27:54 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -3,10 +3,8 @@ const solution = (n, words) => {
   let cnt = 1;
   let arr = [words[0]];
   for (let i = 1; i < words.length; i++) {
-    if (arr.includes(words[i])) return [member, cnt];
+    if (arr.includes(words[i]) || words[i][0] !== words[i - 1][words[i - 1].length - 1]) return [member, cnt];
     else arr.push(words[i]);
-    if (words[i][0] !== words[i - 1][words[i - 1].length - 1])
-      return [member, cnt];
     member++;
     if (member > n) {
       member = 1;
