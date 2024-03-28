b91067d95ba0911a3fb97ce270aec0a970d601ed
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-03-28 18:02:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 32.97 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,13 @@
 const solution = (participant, completion) => {
-  participant.sort();
-  completion.sort();
+  let obj = new Map();
   for (let i = 0; i < participant.length; i++) {
-    if (participant[i] !== completion[i]) return participant[i];
+    obj.has(participant[i])
+      ? obj.set(participant[i], obj.get(participant[i]) + 1)
+      : obj.set(participant[i], 1);
   }
+  for (let i = 0; i < completion.length; i++) {
+    obj.set(completion[i], obj.get(completion[i]) - 1);
+    if (obj.get(completion[i]) === 0) obj.delete(completion[i]);
+  }
+  return [...obj].flat()[0];
 };
