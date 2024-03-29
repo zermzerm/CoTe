dff7c9a37392eaa0e2d1d53f7c2f98218e03aa3a
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-03-29 16:26:31 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -2,12 +2,7 @@ function solution(my_string) {
   let arr = my_string.split(" ");
   let tmp = +arr[0];
   for (let i = 1; i < arr.length; i++) {
-    if (arr[i] === "+") {
-      tmp += +arr[i + 1];
-    }
-    if (arr[i] === "-") {
-      tmp -= +arr[i + 1];
-    }
+    arr[i] === "+" ? (tmp += +arr[i + 1]) : (tmp -= +arr[i + 1]);
     i++;
   }
   return tmp;
