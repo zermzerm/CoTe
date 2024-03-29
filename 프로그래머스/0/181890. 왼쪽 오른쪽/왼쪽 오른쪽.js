5253baa98186de54496e98d1816d1580b91290ac
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-03-29 17:20:44 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,17 @@
+function solution(str_list) {
+  let larr = [],
+    rarr = [];
+      if (str_list.indexOf("l") < 0 && str_list.indexOf("r") < 0) {
+    return [];
+  }
+  for (let i = 0; i < str_list.length; i++) {
+    if (str_list[i] !== "l" && "r") larr.push(str_list[i]);
+    if (str_list[i] === "l") return larr;
+    if (str_list[i] === "r") {
+      for (let j = i + 1; j < str_list.length; j++) {
+        rarr.push(str_list[j]);
+      }
+      return rarr;
+    }
+  }
+}
