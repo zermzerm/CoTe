03da40c395954ecac9c3282cb858d27e660f4b6c
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-04-03 12:47:22 +0900

    [level 1] Title: 모의고사, Time: 6.02 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -2,21 +2,19 @@ const one = [1, 2, 3, 4, 5];
 const two = [2, 1, 2, 3, 2, 4, 2, 5];
 const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
+const compete = (answers, arr, cnt, k) => {
+  for (let i = 0; i < answers.length; i++) {
+    let j = i % arr.length;
+    if (answers[i] === arr[j]) cnt[k]++;
+  }
+};
+
 const solution = (answers) => {
   const cnt = [0, 0, 0];
   const answer = [];
-  for (let i = 0; i < answers.length; i++) {
-    let j = i % one.length;
-    if (answers[i] === one[j]) cnt[0]++;
-  }
-  for (let i = 0; i < answers.length; i++) {
-    let j = i % two.length;
-    if (answers[i] === two[j]) cnt[1]++;
-  }
-  for (let i = 0; i < answers.length; i++) {
-    let j = i % three.length;
-    if (answers[i] === three[j]) cnt[2]++;
-  }
+  compete(answers, one, cnt, 0);
+  compete(answers, two, cnt, 1);
+  compete(answers, three, cnt, 2);
   let max = Math.max(...cnt);
   for (let i = 0; i < cnt.length; i++) {
     if (cnt[i] === max) answer.push(i + 1);
