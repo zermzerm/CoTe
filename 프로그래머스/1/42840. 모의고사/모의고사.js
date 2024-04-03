356a4b76806b2cf4880e078197ebe320573d2339
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-04-03 12:06:06 +0900

    [level 1] Title: 모의고사, Time: 3.57 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,25 @@
+const one = [1, 2, 3, 4, 5];
+const two = [2, 1, 2, 3, 2, 4, 2, 5];
+const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+const solution = (answers) => {
+  const cnt = [0, 0, 0];
+  const answer = [];
+  for (let i = 0; i < answers.length; i++) {
+    let j = i % one.length;
+    if (answers[i] === one[j]) cnt[0]++;
+  }
+  for (let i = 0; i < answers.length; i++) {
+    let j = i % two.length;
+    if (answers[i] === two[j]) cnt[1]++;
+  }
+  for (let i = 0; i < answers.length; i++) {
+    let j = i % three.length;
+    if (answers[i] === three[j]) cnt[2]++;
+  }
+  let max = Math.max(...cnt);
+  for (let i = 0; i < cnt.length; i++) {
+    if (cnt[i] === max) answer.push(i + 1);
+  }
+  return answer;
+};
