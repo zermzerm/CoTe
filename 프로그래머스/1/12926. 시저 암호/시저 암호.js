a20e3cf739882c2f990fac67b6b768820fdb610b
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-04-22 17:07:10 +0900

    [level 1] Title: 시저 암호, Time: 4.02 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,21 +1,26 @@
 function solution(s, n) {
-  //아스키 코드 65 97
-  //문자 -> 아스키코드 = 문자.charCodeAt()
-  //아스키코드 -> 문자 = String.fromCharCode(번호)
-  let arr = s.split("");
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === " ") continue;
-    if (arr[i].charCodeAt() <= 90) {
-      //대문자 65~90
-      let cur = arr[i].charCodeAt() + n;
-      if (cur > 90) cur -= 26;
-      arr.splice(i, 1, String.fromCharCode(cur));
-    } else {
-      //소문자 97~122
-      let cur = arr[i].charCodeAt() + n;
-      if (cur > 122) cur -= 26;
-      arr.splice(i, 1, String.fromCharCode(cur));
+  //대문자 65~90
+  //소문자 97~122
+  let answer = '';
+  s = s.split('');
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == ' ') {
+      answer += ' ';
+      continue;
+    }
+    let newChar = s[i].charCodeAt();
+    //대문자
+    if (newChar <= 90) {
+      newChar += n;
+      if (newChar > 90) newChar -= 26;
+      answer += String.fromCharCode(newChar);
+    }
+    //소문자
+    else {
+      newChar += n;
+      if (newChar > 122) newChar -= 26;
+      answer += String.fromCharCode(newChar);
     }
   }
-  return arr.join("");
+  return answer;
 }
