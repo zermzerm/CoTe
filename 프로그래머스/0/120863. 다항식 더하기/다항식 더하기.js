6d3e55e8662578940969469af92e55292d0e02cc
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-05-08 16:36:33 +0900

    [level 0] Title: 다항식 더하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,24 +1,29 @@
+function calc(prev, num) {
+  if (prev === "+") return +num;
+  else if (prev === "-") return -num;
+  else return +num;
+}
+
 function solution(polynomial) {
-    let exp = polynomial.split(" "),xArr=[],NumArr=[],xNum=0,num=0;
-    exp.forEach((el)=>{
-        if(el.match('x')){
-            console.log(el.slice(el.length-1,1));
-            el.length===1?xNum+=1:xNum+=Number(el.replace('x',''));
-        }
-        else if(el!=='+'){
-            num+=Number(el);
-        }
-    });
-    if(xNum==1){
-        xNum='';
+  const arr = polynomial.split(" ");
+  let xValue = 0,
+    numValue = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes("x")) {
+      let num = +arr[i].replace(/[^0-9]/g, "");
+      if (num === 0) num = 1;
+      xValue += calc(arr[i - 1], num);
+    } else if (Number.isInteger(+arr[i])) {
+      let num = +arr[i];
+      numValue += calc(arr[i - 1], num);
     }
-    if(xNum===0){
-        return `${num}`;
-    }
-    else if(num===0){
-        return `${xNum}x`
-    }
-    else{
-       return `${xNum}x + ${num}`
-    }
+  }
+      if (xValue === 1) xValue = "";
+  if (xValue === 0)
+    return numValue < 0 ? `- ${Math.abs(numValue)}` : `${numValue}`;
+  if (numValue === 0)
+    return xValue < 0 ? `- ${Math.abs(xValue)}x` : `${xValue}x`;
+  return `${xValue < 0 ? "- " + Math.abs(xValue) : xValue}x ${
+    numValue < 0 ? "- " + Math.abs(numValue) : "+ " + numValue
+  }`;
 }
