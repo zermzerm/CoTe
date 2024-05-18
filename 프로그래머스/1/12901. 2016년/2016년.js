87bd8dd01f82797e28b40e137212523d1b58951f
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-05-18 16:45:03 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,10 @@
+const MONTH = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const DAY = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
+
+function solution(a, b) {
+  let mon = b;
+  for (let i = 0; i < a - 1; i++) {
+    mon += MONTH[i];
+  }
+  return mon % 7 === 0 ? DAY.at(-1) : DAY[(mon % 7) - 1];
+}
