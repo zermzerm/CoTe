ffec20eeb9b09319d7a0da1e4f14f0e12dbe9728
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-05-18 16:22:26 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,5 @@
+function solution(common) {
+  if (common[1] - common[0] === common[2] - common[1])
+    return common.at(-1) + common[1] - common[0];
+  else return common.at(-1) * (common[1] / common[0]);
+}
