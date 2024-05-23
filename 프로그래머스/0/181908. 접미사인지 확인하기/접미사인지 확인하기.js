c8e5ae61307f4bab8dcd6a0e049f9d1cb1a8fd58
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-05-23 16:27:57 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,7 +1,3 @@
 function solution(my_string, is_suffix) {
-    const arr =[];
-for(let i=0;i<my_string.length;i++){
-    arr.push(my_string.slice(i))
-}
-    return arr.indexOf(is_suffix) !== -1 ? 1 : 0
+return my_string.endsWith(is_suffix) ? 1 : 0
 }
