7073d0b62c594f25a12658a31b88f85533afe000
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-05-24 20:06:23 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -5,7 +5,6 @@ function solution(lottos, win_nums) {
   for (let i = 0; i < lottos.length - zeroCount; i++) {
     if (win_nums.includes(lottos[i])) sameCount++;
   }
-  if (zeroCount === 6 && sameCount === 0) return [1, 6];
   return [
     7 - (sameCount + zeroCount) > 6 ? 6 : 7 - (sameCount + zeroCount),
     7 - sameCount > 6 ? 6 : 7 - sameCount,
