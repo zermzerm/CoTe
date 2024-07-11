25d13df31db13d883f0a057fa4db20706ff9d511
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-07-11 23:26:47 +0900

    [level 0] Title: 배열 만들기 6, Time: 8.84 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,10 +1,7 @@
 function solution(arr) {
-    const stk =[];
-    for(let i=0; i<arr.length;i++){
-        if(stk.length===0) stk.push(arr[i])
-        else{
-            stk.at(-1) === arr[i] ? stk.pop() : stk.push(arr[i])
-        }
+    const stk =[arr[0]];
+    for(let i=1; i<arr.length;i++){
+            stk.at(-1) === arr[i] ? stk.pop() : stk.push(arr[i])    
     }
     return stk.length!==0 ? stk : [-1]
 }
