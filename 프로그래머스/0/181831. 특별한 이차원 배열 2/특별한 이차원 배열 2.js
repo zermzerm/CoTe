8ac49e202a27c2b0854332f21e656b5939482338
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-08-15 06:47:56 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.88 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,9 +1,8 @@
 function solution(arr) {
-    answer =1;
     for(let i=0;i<arr.length;i++){
         for(let j=0;j<arr.length;j++){
-            if(arr[i][j]!==arr[j][i]) return answer =0;
+            if(arr[i][j]!==arr[j][i]) return 0;
         }
     }
-    return answer
+    return 1
 }
