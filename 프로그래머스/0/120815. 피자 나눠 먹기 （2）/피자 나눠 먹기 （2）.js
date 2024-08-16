b0c8f330fff4bcf42f1964e601aa193664e1dc63
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-08-17 06:18:53 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,11 +1,9 @@
 function solution(n) {
-    let cnt=1;
     let piz=6;
    while(true){
        if(piz%n===0){
-           return cnt;
+           return piz/6;
        }
        piz+=6;
-       cnt++;
    }
 }
