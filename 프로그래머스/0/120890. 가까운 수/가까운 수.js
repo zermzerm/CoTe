edb4e35d5a39cb78605833dc0d9be1198a740d16
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-09-14 05:05:32 +0900

    [level 0] Title: 가까운 수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,3 +1,3 @@
 function solution(array, n) {
-    return array.sort((a,b)=>a-b).sort((a,b)=>Math.abs(a-n)-Math.abs(b-n))[0];
+    return array.sort((a,b)=>Math.abs(a-n)-Math.abs(b-n) || a-b )[0];
 }
