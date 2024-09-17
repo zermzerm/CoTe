c3413dc16675b5150437bf4171761d711eb55a9d
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-09-17 23:45:09 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -1,11 +1,3 @@
-function v(a,b){
-    return a+b>=1 ? true : false
-}
-
-function n(a,b){
-    return a*b===1 ? true : false
-}
-
 function solution(x1, x2, x3, x4) {
-    return n(v(x1,x2),v(x3,x4))
+    return Boolean((x1||x2)*(x3||x4))
 }
