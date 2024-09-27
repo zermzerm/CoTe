2af84cd198f77b39df4f86a3e7e7bea80c238d7a
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-09-27 17:51:29 +0900

    [level 2] Title: 할인 행사, Time: 65.72 ms, Memory: 43 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,18 @@
+function solution(want, number, discount) {
+    let allItem = [];
+    let cnt=0;
+    for(let i=0;i<want.length;i++){
+        for(let j=0;j<number[i];j++){
+            allItem.push(want[i])
+        }
+    }
+    // console.log(allItem)
+    for(let i=0;i<discount.length;i++){
+        let tmpItem = [...allItem];
+        for(let j=i;j<i+10;j++){
+            if(tmpItem.includes(discount[j])) tmpItem.splice(tmpItem.indexOf(discount[j]),1);
+        }
+        if(tmpItem.length===0) cnt++;
+    }
+    return cnt;
+}
