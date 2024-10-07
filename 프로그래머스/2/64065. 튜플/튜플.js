58a521e4ae054552c102d01118e28f25e05135d5
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-10-07 11:05:27 +0900

    [level 2] Title: 튜플, Time: 45.68 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,11 +1,10 @@
 function solution(s) {
-    const arr = s.slice(1,-1).split("},{").map((el)=>el.replace(/{|}/g,'').split(",")).sort((a,b)=>a.length-b.length);
-    // console.log(arr)
-    let answer = [+arr[0]]
-    for(let i=1;i<arr.length;i++){
-        let tmpArr = [...arr[i-1],...arr[i]];
-        let uniqueArr = tmpArr.filter(item => tmpArr.indexOf(item) === tmpArr.lastIndexOf(item));
-        answer.push(+uniqueArr[0])
-    }
-    return answer
+    return arr = s.slice(1,-1).split("},{").map((el)=>el.replace(/{|}/g,'').split(",").map((el)=>+el)).sort((a,b)=>a.length-b.length).reduce((acc,cur)=>[...acc,...cur.filter((el)=>!acc.includes(el))],[]);
+    // let answer = [+arr[0]]
+    // for(let i=1;i<arr.length;i++){
+    //     let tmpArr = [...arr[i-1],...arr[i]];
+    //     let uniqueArr = tmpArr.filter(item => tmpArr.indexOf(item) === tmpArr.lastIndexOf(item));
+    //     answer.push(+uniqueArr[0])
+    // }
+    // return answer
 }
