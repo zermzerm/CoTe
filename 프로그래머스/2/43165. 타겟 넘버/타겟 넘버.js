62bd3b23a0a6f9f5caad375d047c1e98d6597a09
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-10-12 17:08:57 +0900

    [level 2] Title: 타겟 넘버, Time: 13.87 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,21 +1,37 @@
-function generateBinarySequences(length) {
-  const result = [];
-  for (let i = 0; i < Math.pow(2, length); i++) {
-    const binary = i.toString(2).padStart(length, '0').split("").map(Number);
-    result.push(binary);
-  }
-  return result;
+function solution(numbers, target){
+    let answer = 0;
+    function dfs(index,sum){
+        if(index===numbers.length){
+            if(sum===target){
+                answer ++;
+            }
+            return;
+        }
+        dfs(index+1,sum+numbers[index]);
+        dfs(index+1,sum-numbers[index]);
+    }
+    dfs(0,0);
+    return answer;
 }
 
-function solution(numbers, target){
-  let cnt =0;
-  const totalArr = generateBinarySequences(numbers.length);
-  for(let i=0;i<totalArr.length;i++){
-    let sum=0;
-    for(let j=0;j<totalArr[i].length;j++){
-      totalArr[i][j]===0 ? sum+=numbers[j] : sum-=numbers[j];
-    }
-    if(sum===target) cnt++;
-  }
-  return cnt;
-}
+// function generateBinarySequences(length) {
+//   const result = [];
+//   for (let i = 0; i < Math.pow(2, length); i++) {
+//     const binary = i.toString(2).padStart(length, '0').split("").map(Number);
+//     result.push(binary);
+//   }
+//   return result;
+// }
+
+// function solution(numbers, target){
+//   let cnt =0;
+//   const totalArr = generateBinarySequences(numbers.length);
+//   for(let i=0;i<totalArr.length;i++){
+//     let sum=0;
+//     for(let j=0;j<totalArr[i].length;j++){
+//       totalArr[i][j]===0 ? sum+=numbers[j] : sum-=numbers[j];
+//     }
+//     if(sum===target) cnt++;
+//   }
+//   return cnt;
+// }
