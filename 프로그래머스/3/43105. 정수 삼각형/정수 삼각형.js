b8ca3c9166f5a90f04b6f3a31ad1c110720314d0
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-10-13 13:45:54 +0900

    [level 3] Title: 정수 삼각형, Time: 5.05 ms, Memory: 40.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,34 @@
+function solution(triangle) {
+    return Math.max(...triangle.reduce((dp, raw) => {
+            return raw.map((v, i) => {
+              return Math.max((i === 0 ? 0 : dp[i - 1]), i === dp.length ? 0 : dp[i]) + v; 
+            });
+        }, [0]));
+}
+
+// function solution(triangle) {
+//     for(let i=triangle.length-2;i>=0;i--){
+//         for(let j=0;j<triangle[i].length;j++){
+//             triangle[i][j] += Math.max(triangle[i+1][j],triangle[i+1][j+1])
+//         }
+//     }
+    
+//     return triangle[0][0]
+// }
+
+// function solution(triangle) {
+//     const answer =[];
+    
+//     function dp(index,cur,sum){
+//         if(index===triangle.length){
+//             if(!isNaN(sum)){
+//                 answer.push(sum);
+//             }
+//             return;
+//         }
+//         dp(index+1,cur,sum + triangle[index][cur]);
+//         dp(index+1,cur+1, triangle[index][cur+1] && sum + triangle[index][cur+1]);
+//     }
+//     dp(0,0,0);
+//     return Math.max(...answer);
+// }
