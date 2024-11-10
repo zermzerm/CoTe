362df2890f7828cffcfed3165a8b898459388c44
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2024-11-10 19:41:08 +0900

    [level 2] Title: 오픈채팅방, Time: 183.06 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -2,26 +2,13 @@ function solution(record) {
     const answer = [];
     const idObj = {};
     const splitRecord = record.map((el)=>el.split(" "));
-    // console.log(splitRecord)
     for(let i=0;i<splitRecord.length;i++){
-        if(splitRecord[i][0]==='Enter'){
-            idObj[splitRecord[i][1]]=splitRecord[i][2];
-            // answer.push(`${idObj[splitRecord[i][1]]}님이 들어왔습니다.`);
-        }
-        if(splitRecord[i][0]==='Leave'){
-            // answer.push(`${idObj[splitRecord[i][1]]}님이 나갔습니다.`);
-        }
-        if(splitRecord[i][0]==='Change'){
-            idObj[splitRecord[i][1]]=splitRecord[i][2];
-        }
+        if(splitRecord[i][0]==='Enter' || splitRecord[i][0]==='Change') idObj[splitRecord[i][1]]=splitRecord[i][2];
     }
     for(let i=0;i<splitRecord.length;i++){
-        if(splitRecord[i][0]==='Enter'){
-            answer.push(`${idObj[splitRecord[i][1]]}님이 들어왔습니다.`);
-        }
-        if(splitRecord[i][0]==='Leave'){
-            answer.push(`${idObj[splitRecord[i][1]]}님이 나갔습니다.`);
+        if(splitRecord[i][0]==='Enter'||splitRecord[i][0]==='Leave'){
+            answer.push(`${idObj[splitRecord[i][1]]}님이 ${splitRecord[i][0]==='Enter' ? '들어왔습니다.' : '나갔습니다.'}`);
         }
     }
-    return answer
+    return answer;
 }
