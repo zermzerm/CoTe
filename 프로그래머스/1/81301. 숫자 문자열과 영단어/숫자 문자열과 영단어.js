c44d62e99a11de31b2bde65b5ec3d7a40a2cbc28
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-04-04 14:38:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,30 +1,18 @@
-function solution(s) {
-  let arr = s.split("");
-  let answer = "";
-  let tmp = "";
-  let object = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-    zero: 0,
-  };
-  for (let i = 0; i < arr.length; i++) {
-    if (!isNaN(arr[i])) {
-      answer += arr[i];
-    } else {
-      tmp += arr[i];
+const obj = {'zero':0,'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,}
+function solution(s){
+    let tmp = '';
+    let answer = '';
+    for(let i=0;i<s.length;i++){
+        if(isNaN(s[i])){
+            tmp+=s[i];
+            if(!isNaN(obj[tmp])){    
+                answer +=obj[tmp];
+                tmp='';
+            }
+        }
+        else{
+            answer +=s[i];
+        }
     }
-    if (object[tmp] !== undefined) {
-      console.log(object[tmp]);
-      answer += object[tmp];
-      tmp = "";
-    }
-  }
-  return +answer;
+    return +answer;
 }
