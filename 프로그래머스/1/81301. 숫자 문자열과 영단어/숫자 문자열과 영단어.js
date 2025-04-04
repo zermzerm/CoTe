534192c4a33d7e442e534fcd6acfdd373a7e9441
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-04-04 14:45:54 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,18 +1,25 @@
-const obj = {'zero':0,'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,}
+// const obj = {'zero':0,'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,}
+const obj = ['zero','one','two','three','four','five','six','seven','eight','nine'];
 function solution(s){
-    let tmp = '';
-    let answer = '';
-    for(let i=0;i<s.length;i++){
-        if(isNaN(s[i])){
-            tmp+=s[i];
-            if(!isNaN(obj[tmp])){    
-                answer +=obj[tmp];
-                tmp='';
-            }
-        }
-        else{
-            answer +=s[i];
-        }
+    let answer = s
+    for(let i=0;i<obj.length;i++){
+        let tmp = answer.split(obj[i]);
+        answer = tmp.join(i);
     }
-    return +answer;
+    return +answer
+    // let tmp = '';
+    // let answer = '';
+    // for(let i=0;i<s.length;i++){
+    //     if(isNaN(s[i])){
+    //         tmp+=s[i];
+    //         if(!isNaN(obj[tmp])){    
+    //             answer +=obj[tmp];
+    //             tmp='';
+    //         }
+    //     }
+    //     else{
+    //         answer +=s[i];
+    //     }
+    // }
+    // return +answer;
 }
