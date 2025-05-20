518bdab45b94805678953360009c4d2990f466ef
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-05-20 14:15:46 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.30 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,8 +1,17 @@
-const solution = (s) => {
-  const small = [],
-    big = [];
-  for (let i = 0; i < s.length; i++) {
-    s[i] === s[i].toUpperCase() ? big.push(s[i]) : small.push(s[i]);
-  }
-  return small.sort().reverse().join("") + big.sort().reverse().join("");
-};
+// const solution = (s) => {
+//   const small = [],
+//     big = [];
+//   for (let i = 0; i < s.length; i++) {
+//     s[i] === s[i].toUpperCase() ? big.push(s[i]) : small.push(s[i]);
+//   }
+//   return small.sort().reverse().join("") + big.sort().reverse().join("");
+// };
+
+function solution(s){
+    let low = [];
+    let high = [];
+    for(let i=0;i<s.length;i++){
+        s[i]===s[i].toLowerCase() ? low.push(s[i]) : high.push(s[i]);
+    }
+    return low.sort().reverse().join("")+high.sort().reverse().join("");
+}
