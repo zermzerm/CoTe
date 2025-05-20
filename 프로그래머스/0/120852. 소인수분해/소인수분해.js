9d9742e0a82630218de1bb3e1387e83a3b47e9ae
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-05-20 13:28:27 +0900

    [level 0] Title: 소인수분해, Time: 0.26 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,13 +1,24 @@
-const solution = (n) => {
-  let answer = new Set();
-  let index = 2;
-  while (n != 1) {
-    if (n % index === 0) {
-      n /=  index;
-      answer.add(index);
-    } else {
-      index++;
+// const solution = (n) => {
+//   let answer = new Set();
+//   let index = 2;
+//   while (n != 1) {
+//     if (n % index === 0) {
+//       n /=  index;
+//       answer.add(index);
+//     } else {
+//       index++;
+//     }
+//   }
+//   return [...answer];
+// };
+
+function solution(n){
+    let answer=[];
+    for(let i=2;i<=n;i++){
+        while(n%i===0){
+            if(!answer.includes(i)) answer.push(i);
+            n=n/i;
+        }
     }
-  }
-  return [...answer];
-};
+    return answer;
+}
