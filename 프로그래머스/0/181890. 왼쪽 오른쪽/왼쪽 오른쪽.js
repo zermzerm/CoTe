3cc242051db54b71ff3a291487e7aa9be3c5fa63
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-05-20 13:17:19 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,17 +1,22 @@
 function solution(str_list) {
-  let larr = [],
-    rarr = [];
-  if (str_list.indexOf("l") < 0 && str_list.indexOf("r") < 0) {
-    return [];
-  }
-  for (let i = 0; i < str_list.length; i++) {
-    if (str_list[i] !== "l" && "r") larr.push(str_list[i]);
-    if (str_list[i] === "l") return larr;
-    if (str_list[i] === "r") {
-      for (let j = i + 1; j < str_list.length; j++) {
-        rarr.push(str_list[j]);
-      }
-      return rarr;
+  // let larr = [],
+  //   rarr = [];
+  // if (str_list.indexOf("l") < 0 && str_list.indexOf("r") < 0) {
+  //   return [];
+  // }
+  // for (let i = 0; i < str_list.length; i++) {
+  //   if (str_list[i] !== "l" || "r") larr.push(str_list[i]);
+  //   if (str_list[i] === "l") return larr;
+  //   if (str_list[i] === "r") {
+  //     for (let j = i + 1; j < str_list.length; j++) {
+  //       rarr.push(str_list[j]);
+  //     }
+  //     return rarr;
+  //   }
+  // }
+    for(let i=0;i<str_list.length;i++){
+        if(str_list[i]==="l") return str_list.slice(0,i)
+        else if(str_list[i]==="r") return str_list.slice(i+1)
     }
-  }
+    return [];
 }
