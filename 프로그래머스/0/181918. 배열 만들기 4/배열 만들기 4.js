c2a56c5b7f6f0668c45b277cddb566a84ed7a86a
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-05-21 17:37:36 +0900

    [level 0] Title: 배열 만들기 4, Time: 23.04 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -5,4 +5,13 @@ function solution(arr) {
         else stk.push(arr[i])
     }
     return stk
+    // let stk = [arr[0]];
+    // for(let i=1;i<arr.length;i++){
+    //     if(stk.at(-1)<arr[i]) stk.push(arr[i])
+    //     else {
+    //        stk.pop();
+    //        i--;
+    //    }
+    // }
+    // return stk
 }
