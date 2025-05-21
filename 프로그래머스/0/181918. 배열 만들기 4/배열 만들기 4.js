710134363918f5ebe203a0673c7f75a25f8e6cc9
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-05-21 17:40:55 +0900

    [level 0] Title: 배열 만들기 4, Time: 30.55 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -5,6 +5,7 @@ function solution(arr) {
         else stk.push(arr[i])
     }
     return stk
+    // 어떤 걸 더 반복하냐 차이 아래는 더 성능이 떨어진다.
     // let stk = [arr[0]];
     // for(let i=1;i<arr.length;i++){
     //     if(stk.at(-1)<arr[i]) stk.push(arr[i])
