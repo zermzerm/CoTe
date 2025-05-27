565f086fb6fcbd180bd05103eb10775b74971f4b
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-05-27 15:58:58 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 5.06 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 5.17 ms
+메모리: 38 MB, 시간: 5.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 12일 12:19:53
+2025년 05월 27일 15:58:57
 
 ### 문제 설명
 
--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,12 +1,24 @@
 function solution(arr, queries) {
-  let answer = [];
-  for (let i = 0; i < queries.length; i++) {
-    let tmp = [];
-    for (let j = queries[i][0]; j <= queries[i][1]; j++) {
-      if (arr[j] > queries[i][2]) tmp.push(arr[j]);
+  // let answer = [];
+  // for (let i = 0; i < queries.length; i++) {
+  //   let tmp = [];
+  //   for (let j = queries[i][0]; j <= queries[i][1]; j++) {
+  //     if (arr[j] > queries[i][2]) tmp.push(arr[j]);
+  //   }
+  //   if (tmp.length === 0) answer.push(-1);
+  //   else answer.push(Math.min(...tmp));
+  // }
+  // return answer;
+    
+    let result = [];
+    for(let i=0;i<queries.length;i++){
+        let tmp=[];
+        for(let j=queries[i][0];j<=queries[i][1];j++){
+                if(queries[i][2]<arr[j]){
+                    tmp.push(arr[j])
+                }
+            }
+        result.push(tmp.length? Math.min(...tmp) : -1);
     }
-    if (tmp.length === 0) answer.push(-1);
-    else answer.push(Math.min(...tmp));
-  }
-  return answer;
+    return result
 }
