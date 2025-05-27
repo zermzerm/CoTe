401e18e0403e2f6ad0e0d20e9da4c49cccdcdfb0
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-05-27 16:44:21 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,11 +1,23 @@
 const solution = (sides) => {
-  const arr = sides.sort((a, b) => a - b);
-  let cnt = 0;
-  for (let i = arr[1] - arr[0] + 1; i <= arr[1]; i++) {
-    cnt++;
-  }
-  for (let i = arr[1] + arr[0] - 1; i > arr[1]; i--) {
-    cnt++;
-  }
-  return cnt;
+  // const arr = sides.sort((a, b) => a - b);
+  // let cnt = 0;
+  // for (let i = arr[1] - arr[0] + 1; i <= arr[1]; i++) {
+  //   cnt++;
+  // }
+  // for (let i = arr[1] + arr[0] - 1; i > arr[1]; i--) {
+  //   cnt++;
+  // }
+  // return cnt;
+    
+    let sum = sides[0]+sides[1];
+    let max = Math.max(...sides);
+    let min = Math.min(...sides);
+    let result = 0;
+    for(let i=max+1;i<sum;i++){
+        result++;
+    }
+    for(let i=max-min+1;i<=max;i++){
+        result++;
+    }
+    return result;
 };
