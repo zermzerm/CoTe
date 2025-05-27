5f25e3a5befdfb844a6961431fe5a7c518d1d15a
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-05-27 15:40:41 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/README.md
+++ 프로그래머스/1/12918. 문자열 다루기 기본/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 다루기 기본 - 12918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.04 ms
+메모리: 33.4 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 03일 11:21:26
+2025년 05월 27일 15:40:39
 
 ### 문제 설명
 
--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,12 +1,20 @@
 const solution = (s) => {
-  const arr = s.split('');
-  if (s.length === 4 || s.length === 6) {
-    for (let i = 0; i < arr.length; i++) {
-      if (isNaN(arr[i])) {
-        return false;
-      }
+  // const arr = s.split('');
+  // if (s.length === 4 || s.length === 6) {
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (isNaN(arr[i])) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
+  // return false;
+    
+    if(s.length===4 || s.length===6){
+       for(let i=0;i<s.length;i++){
+           if(isNaN(s[i])) return false
+       }
+        return true
     }
-    return true;
-  }
-  return false;
+    else return false
 };
