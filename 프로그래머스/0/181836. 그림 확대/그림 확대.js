b225b5a46fd9531e5a4091c42efd5c1db5349ef3
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-06-04 13:35:27 +0900

    [level 0] Title: 그림 확대, Time: 0.33 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -1,12 +1,24 @@
 function solution(picture, k) {
-  let answer = [];
+//   let answer = [];
 
-  for (let i = 0; i < picture.length; i++) {
-    let tmp = '';
-    for (let j = 0; j < picture[i].length; j++) {
-      tmp += picture[i][j].repeat(k);
+//   for (let i = 0; i < picture.length; i++) {
+//     let tmp = '';
+//     for (let j = 0; j < picture[i].length; j++) {
+//       tmp += picture[i][j].repeat(k);
+//     }
+//     answer.push(Array(k).fill(tmp));
+//   }
+//   return answer;
+    
+    let answer = [];
+    for(let i=0;i<picture.length;i++){
+        let tmp = '';
+        for(let j=0;j<picture[i].length;j++){
+            tmp += picture[i][j].repeat(k);
+        }
+        for(let j=0;j<k;j++){
+          answer.push(tmp);  
+        }
     }
-    answer = answer.concat(Array(k).fill(tmp));
-  }
-  return answer;
+    return answer;
 }
