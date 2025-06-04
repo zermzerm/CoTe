11d899e371dc0128e221425ee0175c95d41f7c26
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-06-04 14:55:05 +0900

    [level 2] Title: 올바른 괄호, Time: 27.30 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,11 +1,19 @@
 function solution(s) {
-  let arr = s.split('');
-  let cnt = 0;
-  if (arr[0] === ')') return false;
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] === '(' ? cnt++ : cnt--;
-      if(cnt<0) return false
-  }
-  if (cnt === 0) return true;
-  else return false;
+  // let arr = s.split('');
+  // let cnt = 0;
+  // if (arr[0] === ')') return false;
+  // for (let i = 0; i < arr.length; i++) {
+  //   arr[i] === '(' ? cnt++ : cnt--;
+  //     if(cnt<0) return false
+  // }
+  // if (cnt === 0) return true;
+  // else return false;
+    
+    if(s[0]===')') return false;
+    let cnt =0;
+    for(let i=0;i<s.length;i++){
+        s[i]==='(' ? cnt++ : cnt--;
+        if(cnt<0) return false;
+    }
+    return cnt===0? true : false;
 }
