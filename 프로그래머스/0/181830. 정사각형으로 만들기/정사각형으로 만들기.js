56ebaaf43c024b345229441ec2cd0dbd4ea53ac0
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-06-05 16:36:19 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 1.00 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,15 +1,33 @@
 function solution(arr) {
- let answer = arr.map(v => [...v]);
-  if (arr.length === arr[0].length) return answer;
-  else if (arr.length > arr[0].length) {
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length - arr[0].length; j++) {
-        answer[i].push(0);
-      }
+ // let answer = arr.map(v => [...v]);
+ //  if (arr.length === arr[0].length) return answer;
+ //  else if (arr.length > arr[0].length) {
+ //    for (let i = 0; i < arr.length; i++) {
+ //      for (let j = 0; j < arr.length - arr[0].length; j++) {
+ //        answer[i].push(0);
+ //      }
+ //    }
+ //  } else if (arr.length < arr[0].length) {
+ //    for (let i = 0; i < arr[0].length - arr.length; i++)
+ //      answer.push(new Array(arr[0].length).fill(0));
+ //  }
+ //  return answer;
+    
+    let diff = arr[0].length-arr.length
+    if(diff===0) return arr;
+    if(diff<0){
+        //배열에 0을 diff만큼 추가
+        for(let i=0;i<arr.length;i++){
+            for(let j=0;j<Math.abs(diff);j++){
+                arr[i].push(0);
+            }
+        }
     }
-  } else if (arr.length < arr[0].length) {
-    for (let i = 0; i < arr[0].length - arr.length; i++)
-      answer.push(new Array(arr[0].length).fill(0));
-  }
-  return answer;
+    else {
+        //arr[0].length의 0000배열을 diff만큼 추가
+        for(let i=0;i<diff;i++){
+            arr.push(Array(arr[0].length).fill(0));
+        }
+    }
+    return arr
 }
