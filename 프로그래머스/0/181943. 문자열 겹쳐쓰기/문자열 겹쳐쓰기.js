a7c2ca8e29ecc9381dc9c61bb8ce8c7b8e1893f7
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-06-06 23:44:13 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,9 @@
 function solution(my_string, overwrite_string, s) {
-  let answer = my_string.split('');
-  answer.splice(s, overwrite_string.length, overwrite_string);
-  return answer.join('');
+  // let answer = my_string.split('');
+  // answer.splice(s, overwrite_string.length, overwrite_string);
+  // return answer.join('');
+    
+    let answer = my_string.split("");
+    answer.splice(s,overwrite_string.length,overwrite_string);
+    return answer.join("")
 }
