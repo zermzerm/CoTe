1bff492808d0f19e6f49fc7f62163e710102eb73
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-06-12 17:58:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,7 +1,11 @@
 function solution(numbers) {
-    var sum=0;
-    for(i=0;i<numbers.length;i++){
-        sum+=numbers[i];
-    }
+    // var sum=0;
+    // for(i=0;i<numbers.length;i++){
+    //     sum+=numbers[i];
+    // }
+    // return 45-sum;
+    
+    let sum = 0;
+    for(let i=0;i<numbers.length;i++) sum+=numbers[i];
     return 45-sum;
 }
