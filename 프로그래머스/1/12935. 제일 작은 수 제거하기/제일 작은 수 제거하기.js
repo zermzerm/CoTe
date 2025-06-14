1a6414bb87357fd580a08cc742363a80b6834c56
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-06-14 13:16:28 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.55 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,10 @@
 function solution(arr) {
-    if(arr.length ===1) return [-1];
-    var tmpArr = [...arr].sort((a,b)=>a-b);
-    var answer = arr.filter((el)=>el!==tmpArr[0])
-    return answer;
+    // if(arr.length ===1) return [-1];
+    // var tmpArr = [...arr].sort((a,b)=>a-b);
+    // var answer = arr.filter((el)=>el!==tmpArr[0])
+    // return answer;
+    
+    if(arr.length===1) return [-1];
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
+    return arr
 }
