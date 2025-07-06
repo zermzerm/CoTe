75fcd98c0f7919f06849ac9dea561d2613024b79
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-07-06 16:13:35 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,7 +1,13 @@
 function solution(food) {
-  let arr = [];
-  for (let i = 1; i < food.length; i++) {
-    arr.push(Array(Math.floor(food[i] / 2) + 1).join(i));
-  }
-  return arr.join("") + "0" + arr.reverse("").join("");
+  // let result = [];
+  // for (let i = 1; i < food.length; i++) {
+  //   result.push(String(i).repeat(Math.floor(food[i] / 2)));
+  // }
+  // return result.join('') + '0' + result.reverse().join('');
+    
+    const answer = [];
+    for(let i=1;i<food.length;i++){
+        answer.push((""+i).repeat(Math.floor(food[i]/2)));
+    }
+    return answer.join("") + "0" + answer.reverse().join("")
 }
