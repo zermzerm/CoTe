fddfb33c00aae850d8565100e7559f3b9f4dac43
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-07-06 16:04:15 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.50 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,17 +1,23 @@
 function solution(numbers) {
-  var answer = [];
-  for (let i = 0; i < numbers.length; i++) {
-    for (j = i + 1; j < numbers.length; j++) {
-      let tmp = numbers[i] + numbers[j];
-      if (!answer.includes(tmp)) {
-        answer.push(tmp);
-      }
-    }
-  }
-  return answer.sort((a, b) => a - b);
+  // var answer = [];
+  // for (let i = 0; i < numbers.length; i++) {
+  //   for (j = i + 1; j < numbers.length; j++) {
+  //     let tmp = numbers[i] + numbers[j];
+  //     if (!answer.includes(tmp)) {
+  //       answer.push(tmp);
+  //     }
+  //   }
+  // }
+  // return answer.sort((a, b) => a - b);
+    
+//     const arr = [[1, 4], [3, 4], [3, 10]];
+//     const xArr = arr.map((el)=>el[0]);
+//     console.log(xArr)
 }
 
-/* Set 쓰기
+
+
+//Set 쓰기
 function solution(numbers) {
   var answer = new Set();
   for (let i = 0; i < numbers.length; i++) {
@@ -21,4 +27,3 @@ function solution(numbers) {
   }
   return [...answer].sort((a, b) => a - b);
 }
-*/
