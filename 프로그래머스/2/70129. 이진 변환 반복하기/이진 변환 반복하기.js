1da0ae887a63e0dc4e8b6189c6d563dce3b2b847
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-08-26 12:30:32 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.89 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,13 +1,25 @@
 function solution(s) {
-  let cnt = 0;
-  let zeroCnt = 0;
-  while (!(s === "1")) {
-    while (s.includes("0")) {
-      s = s.replace("0", "");
-      zeroCnt++;
+  // let cnt = 0;
+  // let zeroCnt = 0;
+  // while (!(s === "1")) {
+  //   while (s.includes("0")) {
+  //     s = s.replace("0", "");
+  //     zeroCnt++;
+  //   }
+  //   s = s.length.toString(2);
+  //   cnt++;
+  // }
+  // return [cnt, zeroCnt];
+    let cnt = [0,0];
+    while(s!=="1"){
+        if(s.includes("0")) cnt[1]+=s.match(/0/g).length;
+        s = s.split("0").join("")
+        // while(s.includes("0")){
+        //     s = s.replace("0","");
+        //     cnt[1]++;
+        // }
+        s = s.length.toString(2);
+        cnt[0]++
     }
-    s = s.length.toString(2);
-    cnt++;
-  }
-  return [cnt, zeroCnt];
+    return cnt
 }
