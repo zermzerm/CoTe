93dca169d67da94211d25f7d0f6a42d85c800279
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-08-26 12:04:13 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,24 +1,33 @@
-function solution(n, m) {
-  let answer = [];
-  let nArr = [],
-    mArr = [];
-  if (m % n === 0) {
-    answer.push(n, m);
-    return answer;
-  }
-  for (let i = 1; i <= n; i++) {
-    if (n % i === 0) nArr.push(i);
-  }
-  for (let i = 1; i <= m; i++) {
-    if (m % i === 0) mArr.push(i);
-  }
-  nArr.sort((a, b) => b - a);
-  for (let i = 0; i < nArr.length; i++) {
-    if (mArr.includes(nArr[i])) {
-      answer.push(nArr[i]);
-      break;
+function GCD(n,m){
+    let gcd = 1;
+    for(let i=2;i<=Math.min(n,m);i++){
+        if(n%i===0&&m%i===0) gcd = i;
     }
-  }
-  answer.push((((n / answer[0]) * m) / answer[0]) * answer[0]);
-  return answer;
+    return gcd
+}
+
+function solution(n, m) {
+  // let answer = [];
+  // let nArr = [],
+  //   mArr = [];
+  // if (m % n === 0) {
+  //   answer.push(n, m);
+  //   return answer;
+  // }
+  // for (let i = 1; i <= n; i++) {
+  //   if (n % i === 0) nArr.push(i);
+  // }
+  // for (let i = 1; i <= m; i++) {
+  //   if (m % i === 0) mArr.push(i);
+  // }
+  // nArr.sort((a, b) => b - a);
+  // for (let i = 0; i < nArr.length; i++) {
+  //   if (mArr.includes(nArr[i])) {
+  //     answer.push(nArr[i]);
+  //     break;
+  //   }
+  // }
+  // answer.push((((n / answer[0]) * m) / answer[0]) * answer[0]);
+  // return answer;
+    return [GCD(n,m),(n*m)/GCD(n,m)]
 }
