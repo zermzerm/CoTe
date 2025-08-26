c32c217c94d290ba429f01f757219ed37f767777
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-08-26 12:01:10 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.63 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,21 @@
-function solution(arr)
-{
-    var answer = [arr[0]];
-
-    for(let i = 1; i < arr.length ; i++) {
-        if(arr[i-1] !== arr[i]) answer.push(arr[i]);
-    }
-
-    return answer;
+function solution(arr) {
+//   let answer = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== arr[i + 1]) {
+// answer.push(arr[i])
+//     }
+//   }
+//   return answer;
+    
+    // let stack = [arr[0]];
+    // for(let i=1;i<arr.length;i++){
+    //     let last = stack.pop();
+    //     stack.push(last);
+    //     if(last!==arr[i]){
+    //         stack.push(arr[i]);
+    //     }
+    // }
+    // return stack
+    
+    return arr.filter((el,idx)=>el!==arr[idx+1]);
 }
