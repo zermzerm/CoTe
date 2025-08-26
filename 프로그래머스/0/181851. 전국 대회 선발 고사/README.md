0218b93ed03dc9531f4e568654458d216998c443
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-08-26 12:24:11 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.21 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/README.md
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/README.md
@@ -1,10 +1,10 @@
 # [level 0] 전국 대회 선발 고사 - 181851 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181851?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181851) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.14 ms
+메모리: 33.1 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 09일 17:08:16
+2025년 08월 26일 12:24:11
 
 ### 문제 설명
 
--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,8 +1,14 @@
 function solution(rank, attendance) {
-  const answer = [];
-  for (let i = 1; i <= rank.length; i++) {
-    if (answer.length === 3) break;
-    if (attendance[rank.indexOf(i)]) answer.push(rank.indexOf(i));
-  }
-  return answer[0] * 10000 + answer[1] * 100 + answer[2];
+  // const answer = [];
+  // for (let i = 1; i <= rank.length; i++) {
+  //   if (answer.length === 3) break;
+  //   if (attendance[rank.indexOf(i)]) answer.push(rank.indexOf(i));
+  // }
+  // return answer[0] * 10000 + answer[1] * 100 + answer[2];
+    
+    // let filteredRank = rank.filter((el,idx)=>attendance[idx]).sort((a,b)=>a-b);
+    // return rank.indexOf(filteredRank[0])*10000+100*rank.indexOf(filteredRank[1])+rank.indexOf(filteredRank[2])
+    
+    let filteredRank = rank.map((a,i)=>[a,i]).filter((_,idx)=>attendance[idx]).sort((a,b)=>a[0]-b[0]);
+    return 10000*filteredRank[0][1] + 100*filteredRank[1][1] + filteredRank[2][1]
 }
