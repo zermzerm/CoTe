6ea4861898c6cd37f6917b9ad11f80f11423f7c4
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-01 14:52:27 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.45 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,14 +1,36 @@
 function solution(k, score) {
-  const arr = [];
-  const result = [];
-  for (let i = 0; i < score.length; i++) {
-    if (arr.length <k) arr.push(score[i]);
-    else {
-      arr.sort((a, b) => a - b);
-      if (arr[0] < score[i]) arr.splice(0, 1, score[i]);
+//   const arr = [];
+//   const result = [];
+//   for (let i = 0; i < score.length; i++) {
+//     if (arr.length <k) arr.push(score[i]);
+//     else {
+//       arr.sort((a, b) => a - b);
+//       if (arr[0] < score[i]) arr.splice(0, 1, score[i]);
+//     }
+    
+//     result.push(Math.min(...arr));
+//   }
+//   return result;
+    
+    const arr = [score[0]];
+    let lastScore = score[0];
+    const result = [score[0]];
+    for(let i =1;i<score.length;i++){
+        if(arr.length<k){
+             arr.push(score[i]);
+           arr.sort((a,b)=>b-a);
+            lastScore = arr.at(-1);
+        }
+        else{
+            if(score[i]>lastScore) {
+             arr.pop();
+           arr.push(score[i]);
+           arr.sort((a,b)=>b-a);
+           lastScore = arr.at(-1);
+       }
+        }
+       
+        result.push(lastScore);
     }
-    arr.sort((a, b) => a - b);
-    result.push(arr[0]);
-  }
-  return result;
+    return result
 }
