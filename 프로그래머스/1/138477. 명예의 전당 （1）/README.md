79358eed39adc08d0afe3551e75654ec52c69379
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-01 14:55:37 +0900

    [level 1] Title: 명예의 전당 (1), Time: 5.00 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/1/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477#) 
 
 ### 성능 요약
 
-메모리: 34.1 MB, 시간: 2.45 ms
+메모리: 36.3 MB, 시간: 5.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 01일 14:52:29
+2025년 09월 01일 14:55:38
 
 ### 문제 설명
 
--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,36 +1,36 @@
 function solution(k, score) {
-//   const arr = [];
-//   const result = [];
-//   for (let i = 0; i < score.length; i++) {
-//     if (arr.length <k) arr.push(score[i]);
-//     else {
-//       arr.sort((a, b) => a - b);
-//       if (arr[0] < score[i]) arr.splice(0, 1, score[i]);
-//     }
-    
-//     result.push(Math.min(...arr));
-//   }
-//   return result;
-    
-    const arr = [score[0]];
-    let lastScore = score[0];
-    const result = [score[0]];
-    for(let i =1;i<score.length;i++){
-        if(arr.length<k){
-             arr.push(score[i]);
-           arr.sort((a,b)=>b-a);
-            lastScore = arr.at(-1);
-        }
-        else{
-            if(score[i]>lastScore) {
-             arr.pop();
-           arr.push(score[i]);
-           arr.sort((a,b)=>b-a);
-           lastScore = arr.at(-1);
-       }
-        }
-       
-        result.push(lastScore);
+  const arr = [];
+  const result = [];
+  for (let i = 0; i < score.length; i++) {
+    if (arr.length <k) arr.push(score[i]);
+    else {
+      arr.sort((a, b) => a - b);
+      if (arr[0] < score[i]) arr.splice(0, 1, score[i]);
     }
-    return result
+    
+    result.push(Math.min(...arr));
+  }
+  return result;
+    
+//     const arr = [score[0]];
+//     let lastScore = score[0];
+//     const result = [score[0]];
+//     for(let i =1;i<score.length;i++){
+//         if(arr.length<k){
+//              arr.push(score[i]);
+//            arr.sort((a,b)=>b-a);
+//             lastScore = arr.at(-1);
+//         }
+//         else{
+//             if(score[i]>lastScore) {
+//              arr.pop();
+//            arr.push(score[i]);
+//            arr.sort((a,b)=>b-a);
+//            lastScore = arr.at(-1);
+//        }
+//         }
+       
+//         result.push(lastScore);
+//     }
+//     return result
 }
