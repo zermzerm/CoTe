b14d9b0158ad2e880236675e6e463a2cbe4f1d76
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-01 14:16:18 +0900

    [level 1] Title: 콜라 문제, Time: 0.57 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,10 +1,18 @@
 const solution = (a, b, n) => {
-  let answer = 0;
-  while (n >= 2) {
-    if (n < a) return answer;
-    const returnCoke = Math.floor(n / a) * b;
-    answer += returnCoke;
-    n = returnCoke + (n % a);
-  }
-  return answer;
+  // let answer = 0;
+  // while (n >= 2) {
+  //   if (n < a) return answer;
+  //   const returnCoke = Math.floor(n / a) * b;
+  //   answer += returnCoke;
+  //   n = returnCoke + (n % a);
+  // }
+  // return answer;
+    
+    let cnt = 0;  
+    while(n>=a){
+        const share = Math.floor(n/a)*b;
+        cnt+=share;
+        n=share + (n%a);
+    }
+    return cnt;
 };
