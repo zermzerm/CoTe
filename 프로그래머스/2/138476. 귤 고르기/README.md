57e1b8e7b3bb321275e4466d3cd799d6a3840583
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-02 13:27:34 +0900

    [level 2] Title: 귤 고르기, Time: 180.51 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 55.5 MB, 시간: 426.39 ms
+메모리: 76.6 MB, 시간: 180.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 25일 15:53:24
+2025년 09월 02일 13:27:35
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,21 +1,34 @@
 function solution(k, tangerine) {
-    let obj = {}
+    // let obj = {}
+    // for(let i=0;i<tangerine.length;i++){
+    //     obj[tangerine[i]] ? obj[tangerine[i]]++ : obj[tangerine[i]]=1
+    // }
+    // const sortedEntries = Object.entries(obj).sort((a,b)=>b[1]-a[1]);
+    // let tmp =[]
+    // for(let i=0;i<sortedEntries.length;i++){
+    //     for(let j=0;j<sortedEntries[i][1];j++){
+    //         if(tmp.length===k){
+    //             let answer = new Set(tmp);
+    //             return answer.size;
+    //         }
+    //         else{
+    //             tmp.push(sortedEntries[i])
+    //         }
+    //     }
+    // }
+    // let answer = new Set(tmp);
+    // return answer.size;
+    
+    let obj = {};
+    let cnt=0;
     for(let i=0;i<tangerine.length;i++){
-        obj[tangerine[i]] ? obj[tangerine[i]]++ : obj[tangerine[i]]=1
+        obj[tangerine[i]] ? obj[tangerine[i]]++ : obj[tangerine[i]]=1;
     }
-    const sortedEntries = Object.entries(obj).sort((a,b)=>b[1]-a[1]);
-    let tmp =[]
-    for(let i=0;i<sortedEntries.length;i++){
-        for(let j=0;j<sortedEntries[i][1];j++){
-            if(tmp.length===k){
-                let answer = new Set(tmp);
-                return answer.size;
-            }
-            else{
-                tmp.push(sortedEntries[i])
-            }
-        }
+    let arr = Object.entries(obj).sort((a,b)=>b[1]-a[1]);
+    for(let i=0;i<arr.length;i++){
+        k-=arr[i][1];
+        cnt++;
+        if(k<=0) return cnt;
     }
-    let answer = new Set(tmp);
-    return answer.size;
+    return cnt;
 }
