dfc7baf5ed4561c48b6dfc54e625aca436c2f1bd
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-02 14:54:05 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,8 +1,17 @@
 function solution(n) {
-  if(n===1) return 1;
-  let arr = [1,1,2];
-  for(let i = 2;i < n; i++){
-    arr.push((arr[i-1]+arr[i])%1234567);
-  }
-  return arr.at(-1)
+  // if(n===1) return 1;
+  // let arr = [1,1,2];
+  // for(let i = 2;i < n; i++){
+  //   arr.push((arr[i-1]+arr[i])%1234567);
+  // }
+  // return arr.at(-1)
+    
+  // 피보나치 수열
+    let dp=[];
+    dp[1]=1;
+    dp[2]=2;
+    for(let i=3;i<=n;i++){
+        dp[i] = (dp[i-2]+dp[i-1])%1234567;
+    }
+    return dp[n]
 }
