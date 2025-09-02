a014cd2cd5b5847bbe1b93b0e0ef83a84d6b81c2
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-02 11:54:08 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,12 +1,21 @@
 function solution(n) {
-  let cnt = 0;
-  while (n !== 0) {
-    if (n % 2 !== 0) {
-      n--;
-      cnt++;
-    } else {
-      n = n / 2;
+  // let cnt = 0;
+  // while (n !== 0) {
+  //   if (n % 2 !== 0) {
+  //     n--;
+  //     cnt++;
+  //   } else {
+  //     n = n / 2;
+  //   }
+  // }
+  // return cnt;
+    let cnt=0;
+    while(n!==0){
+        if(n%2===0) n /= 2;
+        else{
+            n--;
+            cnt++;
+        }
     }
-  }
-  return cnt;
+    return cnt;
 }
