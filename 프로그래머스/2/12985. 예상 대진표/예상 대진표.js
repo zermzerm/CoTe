74dc19fb21fb16e982b17485aad2ff9f684a8841
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-03 15:15:28 +0900

    [level 2] Title: 예상 대진표, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,11 +1,18 @@
-function solution(n,a,b)
-{
-    let answer = 0;
-    while(a !== b) {
-        a = Math.ceil(a/2);
-        b = Math.ceil(b/2);
-        answer++;
+function solution(n, A, B) {
+  // let cnt = 1;
+  // if (a > b) [a, b] = [b, a];
+  // while (true) {
+  //   if (a % 2 === 1 && b - a === 1) break;
+  //   a = Math.ceil(a / 2);
+  //   b = Math.ceil(b / 2);
+  //   cnt++;
+  // }
+  // return cnt;
+    let cnt = 1;
+    while(Math.ceil(A/2)!==Math.ceil(B/2)){
+        A = Math.ceil(A/2);
+        B = Math.ceil(B/2);
+        cnt++;
     }
-
-    return answer;
+    return cnt;
 }
