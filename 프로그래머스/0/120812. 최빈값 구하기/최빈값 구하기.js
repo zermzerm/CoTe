c96e0947ed3eb216cc801da9d260182e6cbbb231
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-03 16:12:52 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -9,8 +9,8 @@ function solution(array) {
     //     let array2 = array.filter((el,i)=>array.indexOf(el)!==i);
     //     array=array2;
     // }
-    if(array.length===1) return +array[0];
-    if(array.length===0) return -1;
+    // if(array.length===1) return +array[0];
+    // if(array.length===0) return -1;
     let obj = {};
     for(let i =0; i<array.length;i++){
         obj[array[i]] ? obj[array[i]]++ : obj[array[i]] = 1;
