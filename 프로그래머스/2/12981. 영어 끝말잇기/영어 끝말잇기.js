22d4e072223ac6734d7cd284a84fc948f104fa98
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-03 15:07:40 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,15 +1,29 @@
 const solution = (n, words) => {
-  let member = 2;
-  let cnt = 1;
-  let arr = [words[0]];
-  for (let i = 1; i < words.length; i++) {
-    if (arr.includes(words[i]) || words[i][0] !== words[i - 1][words[i - 1].length - 1]) return [member, cnt];
-    else arr.push(words[i]);
-    member++;
-    if (member > n) {
-      member = 1;
-      cnt++;
+  // let member = 2;
+  // let cnt = 1;
+  // let arr = [words[0]];
+  // for (let i = 1; i < words.length; i++) {
+  //   if (arr.includes(words[i]) || words[i][0] !== words[i - 1][words[i - 1].length - 1]) return [member, cnt];
+  //   else arr.push(words[i]);
+  //   member++;
+  //   if (member > n) {
+  //     member = 1;
+  //     cnt++;
+  //   }
+  // }
+  // return [0, 0];
+    
+    
+    let answer = [1,1];
+    let acc = words[0];
+    for(let i=1;i<words.length;i++){
+        if(i%n===0) answer[1]++;
+        let cur = words[i];   
+        if(acc[acc.length-1]!==cur[0]||words.indexOf(cur)!==i){
+            answer[0] = i%n+1;
+            return answer;
+        }
+        acc = words[i];
     }
-  }
-  return [0, 0];
+        return [0,0]
 };
