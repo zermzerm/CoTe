c39a9ebdec564a1cbe5100ae673f159611bf7c27
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-04 13:01:32 +0900

    [level 2] Title: 할인 행사, Time: 119.02 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,17 +1,28 @@
 function solution(want, number, discount) {
-    let allItem = [];
-    let cnt=0;
-    for(let i=0;i<want.length;i++){
-        for(let j=0;j<number[i];j++){
-            allItem.push(want[i])
-        }
-    }
-    for(let i=0;i<discount.length;i++){
-        let tmpItem = [...allItem];
+    // let allItem = [];
+    // let cnt=0;
+    // for(let i=0;i<want.length;i++){
+    //     for(let j=0;j<number[i];j++){
+    //         allItem.push(want[i])
+    //     }
+    // }
+    // for(let i=0;i<discount.length;i++){
+    //     let tmpItem = [...allItem];
+    //     for(let j=i;j<i+10;j++){
+    //         if(tmpItem.includes(discount[j])) tmpItem.splice(tmpItem.indexOf(discount[j]),1);
+    //     }
+    //     if(tmpItem.length===0) cnt++;
+    // }
+    // return cnt;
+    
+    
+    let cnt = 0;
+    for(let i=0;i<=discount.length-10;i++){
+        let wantNum = Array(want.length).fill(0);
         for(let j=i;j<i+10;j++){
-            if(tmpItem.includes(discount[j])) tmpItem.splice(tmpItem.indexOf(discount[j]),1);
+            if(want.includes(discount[j])) wantNum[want.indexOf(discount[j])]++;
         }
-        if(tmpItem.length===0) cnt++;
+        if(JSON.stringify(wantNum)===JSON.stringify(number)) cnt++;
     }
-    return cnt;
+    return cnt
 }
