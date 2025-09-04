6b10d6e8a640c6f905975bba2bfbfdaac0ec5f8f
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-04 14:12:25 +0900

    [level 2] Title: 괄호 회전하기, Time: 23.58 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,23 +1,42 @@
 function solution(s){
-    const obj = {']' : '[',')' : '(','}' : '{'};
-    let cnt =0;
-    for(let j=0;j<s.length;j++){
-        const stack = [];
-        for(let i=0;i<s.length;i++){
-            if(s[i]===']' || s[i]==='}' || s[i]===')'){
-                let popString = stack.at(-1);
-                if(obj[s[i]]===popString) {
-                    stack.pop();
-                }
-                else {
-                    stack.push(-1);
-                    break;
-                }
+    // const obj = {']' : '[',')' : '(','}' : '{'};
+    // let cnt =0;
+    // for(let j=0;j<s.length;j++){
+    //     const stack = [];
+    //     for(let i=0;i<s.length;i++){
+    //         if(s[i]===']' || s[i]==='}' || s[i]===')'){
+    //             let popString = stack.at(-1);
+    //             if(obj[s[i]]===popString) {
+    //                 stack.pop();
+    //             }
+    //             else {
+    //                 stack.push(-1);
+    //                 break;
+    //             }
+    //         }
+    //         else stack.push(s[i])
+    //     }
+    //     if(stack.length===0) cnt++;
+    //     s = s[s.length - 1] + s.slice(0,-1) 
+    // }
+    // return cnt;
+    
+    let obj = {'(' : ')', '{':'}', "[":']'}
+    let answer = 0;
+    for(let i=0;i<s.length;i++){
+        let cur = s.slice(i)+s.slice(0,i);
+        let stack = [];
+        let cnt = 0
+        for(let j=0;j<cur.length;j++){
+            if(cur[j]==='{' || cur[j]==='(' || cur[j]==='[') stack.push(cur[j])     
+            else {
+                if(stack.length===0) break;
+                let prev = stack.pop();
+                if(obj[prev]!==cur[j]) break;
             }
-            else stack.push(s[i])
+            cnt++;
         }
-        if(stack.length===0) cnt++;
-        s = s[s.length - 1] + s.slice(0,-1) 
+        if(cnt===cur.length && stack.length===0) answer++;
     }
-    return cnt;
+    return answer
 }
