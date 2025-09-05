aa1369245d2441e6fda420636b080d465853f4cd
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-05 13:03:11 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.35 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,11 +1,25 @@
 function solution(arr1, arr2) {
-    const answer = Array.from({length:arr1.length}, () => Array(arr2[0].length).fill(0));
+    // const answer = Array.from({length:arr1.length}, () => Array(arr2[0].length).fill(0));
+    // for(let i=0;i<arr1.length;i++){
+    //     for(let j=0;j<arr1[i].length;j++){
+    //         for(let k=0;k<arr2[0].length;k++){
+    //             answer[i][k] += arr1[i][j]*arr2[j][k]
+    //         }
+    //     }
+    // }
+    // return answer;
+    
+    let answer = [];
     for(let i=0;i<arr1.length;i++){
-        for(let j=0;j<arr1[i].length;j++){
-            for(let k=0;k<arr2[0].length;k++){
-                answer[i][k] += arr1[i][j]*arr2[j][k]
+        let tmp = [];
+        for(let j=0;j<arr2[0].length;j++){
+            let multi = 0;
+            for(let k=0;k<arr1[i].length;k++){
+                multi+=arr1[i][k]*arr2[k][j]
             }
+            tmp.push(multi);
         }
+        answer.push(tmp)
     }
     return answer;
 }
