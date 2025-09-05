a2467c5e3a705fafef02fe57a231ea3abf571051
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-05 13:43:36 +0900

    [level 1] Title: 폰켓몬, Time: 0.57 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -4,7 +4,7 @@ const solution = (nums) => {
   //   0
   // );
     
-    let dedupliNumsLeng = [...new Set(nums)].length;
+    let dedupliNumsLeng = new Set(nums).size;
     let leng = nums.length/2;
     return dedupliNumsLeng > leng ? leng : dedupliNumsLeng;
     // 1 2 3 4 5 6
