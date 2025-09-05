a0bdac1b949a6a1c7a42fffb1f7ac7a3553aee38
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-05 13:55:34 +0900

    [level 1] Title: 기사단원의 무기, Time: 218.50 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,19 +1,36 @@
-function measure(number) {
-  let cnt = 0;
-  for (let i = 1; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) {
-      cnt++;
-      if (i != number / i) cnt++;
+// function measure(number) {
+//   let cnt = 0;
+//   for (let i = 1; i <= number**0.5; i++) {
+//     if (number % i === 0) {
+//       cnt++;
+//       if (i != number / i) cnt++;
+//     }
+//   }
+//   return cnt;
+// }
+
+function measure (num){
+    let cnt=0;
+    for(let i=1;i<=num**0.5;i++){
+        if(num%i===0){
+             cnt++;
+            if(i!==num/i) cnt++;
+        }
     }
-  }
-  return cnt;
+    return cnt;
 }
 
 function solution(number, limit, power) {
-  let result = 0;
-  for (let i = 1; i <= number; i++) {
-    if (measure(i) > limit) result += power;
-    else result += measure(i);
-  }
-  return result;
+  // let result = 0;
+  // for (let i = 1; i <= number; i++) {
+  //   if (measure(i) > limit) result += power;
+  //   else result += measure(i);
+  // }
+  // return result;
+    
+    let arr = 0;
+    for(let i=1;i<=number;i++){
+        arr+=measure(i)>limit ? power : measure(i);
+    }
+    return arr;
 }
