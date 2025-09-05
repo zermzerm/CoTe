0fb819db06939735b5565ef05ba1abc1bf2a58c9
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-05 13:24:25 +0900

    [level 2] Title: H-Index, Time: 154.76 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,13 +1,21 @@
 function solution(citations) {
-    let cnt=0;
-    while(true){
-        let tmpCnt=0;
-        for(let i=0;i<citations.length;i++){
-            if(citations[i]>=cnt) tmpCnt++;
-        }
-        let lowCnt = citations.filter((el)=>el<cnt).length;
-        if(cnt>tmpCnt) break;
-        cnt++;
+    // let cnt=0;
+    // while(true){
+    //     let tmpCnt=0;
+    //     for(let i=0;i<citations.length;i++){
+    //         if(citations[i]>=cnt) tmpCnt++;
+    //     }
+    //     let lowCnt = citations.filter((el)=>el<cnt).length;
+    //     if(cnt>tmpCnt) break;
+    //     cnt++;
+    // }
+    // return cnt-1;
+    
+    
+    let cnt = 0;
+    for(let i=0;i<=Math.max(...citations);i++){
+        let compare = citations.filter((el)=>el>=i).length;
+        if(i<=compare) cnt = i
     }
-    return cnt-1;
+    return cnt;
 }
