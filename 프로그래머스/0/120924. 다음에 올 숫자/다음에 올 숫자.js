ba147a594b8b49d00c4b9d96c223bbb45d01bf24
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-05 12:27:49 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,5 +1,8 @@
 function solution(common) {
-  if (common[1] - common[0] === common[2] - common[1])
-    return common.at(-1) + common[1] - common[0];
-  else return common.at(-1) * (common[1] / common[0]);
+  // if (common[1] - common[0] === common[2] - common[1])
+  //   return common.at(-1) + common[1] - common[0];
+  // else return common.at(-1) * (common[1] / common[0]);
+    
+    if(common[1]-common[0]===common[2]-common[1]) return 2*common[common.length-1]-common[common.length-2];
+    else return common[common.length-1]**2/common[common.length-2]
 }
