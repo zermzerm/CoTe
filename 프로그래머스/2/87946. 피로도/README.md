fed9aa6c2be46a779bfb71b273736c82bb540066
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-11 13:00:41 +0900

    [level 2] Title: 피로도, Time: 5.70 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 68 MB, 시간: 95.25 ms
+메모리: 36.3 MB, 시간: 5.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 08일 11:55:43
+2025년 09월 11일 13:00:40
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,35 +1,55 @@
 function solution(k, dungeons) {
-    dungeons.sort((a,b)=>(b[0]-b[1])-(a[0]-a[1]));
-    const answer = [];
-    const permutations = getPermutations(dungeons, dungeons.length);
-    for(let i=0;i<permutations.length;i++){
-        let cnt=0;
-        let tmpK=k;
-        for(let j=0;j<permutations[i].length;j++){
-            if(permutations[i][j][0]<=tmpK){
-            tmpK-=permutations[i][j][1];
-            cnt++;
+    const N = dungeons.length;
+    const visited = Array(N).fill(0);
+    let answer =0;
+    
+    function dfs(k,cnt){
+        answer = Math.max(answer,cnt)
+        
+        for(let i=0;i<N;i++){
+            if(k>=dungeons[i][0] && !visited[i]){
+                visited[i]=1;
+                dfs(k-dungeons[i][1], cnt+1)
+                visited[i]=0;
+            }
         }
-        }
-        answer.push(cnt)
     }
-    return Math.max(...answer);
+    dfs(k,0);
+    return answer;
 }
 
-function getPermutations(arr, n) {
-  let results = [];
+// function solution(k, dungeons) {
+//     dungeons.sort((a,b)=>(b[0]-b[1])-(a[0]-a[1]));
+//     const answer = [];
+//     const permutations = getPermutations(dungeons, dungeons.length);
+//     for(let i=0;i<permutations.length;i++){
+//         let cnt=0;
+//         let tmpK=k;
+//         for(let j=0;j<permutations[i].length;j++){
+//             if(permutations[i][j][0]<=tmpK){
+//             tmpK-=permutations[i][j][1];
+//             cnt++;
+//         }
+//         }
+//         answer.push(cnt)
+//     }
+//     return Math.max(...answer);
+// }
 
-  if (n === 1) {
-      return arr.map(item => [item]);
-  }
+// function getPermutations(arr, n) {
+//   let results = [];
 
-  arr.forEach((current, index) => {
-      const remaining = arr.slice(0, index).concat(arr.slice(index + 1));
-      const smallerPermutations = getPermutations(remaining, n - 1);
-      smallerPermutations.forEach(permutation => {
-          results.push([current, ...permutation]);
-      });
-  });
+//   if (n === 1) {
+//       return arr.map(item => [item]);
+//   }
 
-  return results;
-}
+//   arr.forEach((current, index) => {
+//       const remaining = arr.slice(0, index).concat(arr.slice(index + 1));
+//       const smallerPermutations = getPermutations(remaining, n - 1);
+//       smallerPermutations.forEach(permutation => {
+//           results.push([current, ...permutation]);
+//       });
+//   });
+
+//   return results;
+// }
