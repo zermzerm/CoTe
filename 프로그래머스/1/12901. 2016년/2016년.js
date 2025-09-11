f43e0433e6d9ae2ff4eaf2dd3f996758f0330ca9
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-11 13:58:35 +0900

    [level 1] Title: 2016년, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -7,7 +7,6 @@ function solution(a, b) {
   //   mon += MONTH[i];
   // }
   // return mon % 7 === 0 ? DAY.at(-1) : DAY[(mon % 7) - 1];
-    let day = (MONTH.reduce((acc,cur,idx)=>idx<a-1 ? acc+cur : acc,0)+b)%7
-    return DAY[day ? day-1 : 6];
+    return DAY.at((MONTH.reduce((acc,cur,idx)=>idx<a-1 ? acc+cur : acc,0)+b)%7-1)
     
 }
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,6 +1,6 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 11일 13:56:38
+2025년 09월 11일 13:58:34
 
 ### 문제 설명
 
