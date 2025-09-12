052011e31864ca7649b61ef728e8e748077e2616
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-12 17:25:15 +0900

    [level 2] Title: 기능개발, Time: 3.95 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,20 +1,40 @@
 function solution(progresses, speeds) {
-  let calc = [];
-  let answer = [];
-  let cnt = 1;
-  for (let i = 0; i < progresses.length; i++) {
-    calc.push(Math.ceil((100 - progresses[i]) / speeds[i]));
-  }
-  for (let i = 0; i < calc.length; i++) {
-    if (calc[i] >= calc[i + 1]) {
-      cnt++;
-      calc[i + 1] = calc[i];
-    } else if (calc[i] < calc[i + 1]) {
-      answer.push(cnt);
-      cnt = 1;
-      if (calc[i] >= calc[i + 1]) cnt++;
+  // let calc = [];
+  // let answer = [];
+  // for (let i = 0; i < progresses.length; i++) {
+  //   calc.push(Math.ceil((100 - progresses[i]) / speeds[i]));
+  // }
+  // let stack = [calc[0]];
+  // for (let i = 1; i < calc.length; i++) {
+  //   if (stack[stack.length - 1] >= calc[i]) stack.unshift(calc[i]);
+  //   else {
+  //     answer.push(stack.length);
+  //     stack = [calc[i]];
+  //   }
+  //   if (i === calc.length - 1) answer.push(stack.length);
+  // }
+  // return answer;
+    
+    let speedArr = [];
+    for(let i=0;i<progresses.length;i++){
+        speedArr.push(Math.ceil((100-progresses[i])/speeds[i]));
     }
-    if (i === calc.length - 1) answer.push(cnt);
-  }
-  return answer;
+    let cntArr = [speedArr[0]];
+    for(let i=1;i<speedArr.length;i++){
+        if(speedArr[i]<=cntArr[i-1]) cntArr.push(cntArr[i-1])
+        else cntArr.push(speedArr[i])
+    }
+    console.log(speedArr);
+    console.log(cntArr)
+    let answer = [];
+    let cnt =1;
+    for(let i=1;i<cntArr.length;i++){
+        if(cntArr[i-1]!==cntArr[i]){
+            answer.push(cnt);
+            cnt=1;
+        }
+        else cnt++;
+    }
+    answer.push(cnt)
+    return answer
 }
