c09154e1716323310139ee831236b1aef78a99a2
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-16 13:24:45 +0900

    [level 2] Title: [1차] 캐시, Time: 37.36 ms, Memory: 45.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,24 +1,48 @@
 function solution(cacheSize, cities) {
-    const upperCities = cities.map((el)=>el.toUpperCase());
-    const cache = [];
-    let runTime = 0;
-    for(let i=0;i<upperCities.length;i++){
-        if(cacheSize===0){
-            runTime+=5;
-        }
-        else if(cache.includes(upperCities[i])){
-            cache.splice(cache.indexOf(upperCities[i]),1);
-            cache.unshift(upperCities[i]);
-            runTime++;
+    // const upperCities = cities.map((el)=>el.toUpperCase());
+    // const cache = [];
+    // let runTime = 0;
+    // for(let i=0;i<upperCities.length;i++){
+    //     if(cacheSize===0){
+    //         runTime+=5;
+    //     }
+    //     else if(cache.includes(upperCities[i])){
+    //         cache.splice(cache.indexOf(upperCities[i]),1);
+    //         cache.unshift(upperCities[i]);
+    //         runTime++;
+    //     }
+    //     else{
+    //         if(cache.length===cacheSize){
+    //             cache.pop();
+    //         }
+    //         cache.unshift(upperCities[i]); 
+    //         runTime+=5;
+    //     }
+    // }
+    // return runTime;
+    
+    if(cacheSize===0) return cities.length*5;
+    const cacheArr = [];
+    let time = 0;
+    const city = cities.map((el)=>el.toLowerCase());
+    for(let i=0;i<city.length;i++){
+        if(cacheArr.includes(city[i])){
+            time+=1;
+            cacheArr.splice(cacheArr.indexOf(city[i]),1);
+            cacheArr.push(city[i]);
         }
         else{
-            if(cache.length===cacheSize){
-                cache.pop();
+            if(cacheArr.length<cacheSize){
+                cacheArr.push(city[i]);
+                time+=5;
             }
-            cache.unshift(upperCities[i]); 
-            runTime+=5;
+            else{
+                cacheArr.shift();
+                cacheArr.push(city[i]);
+                time+=5;
+            }
         }
     }
-    return runTime;
+    return time;
 }
 
