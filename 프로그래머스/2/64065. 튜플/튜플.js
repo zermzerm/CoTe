3a13600a2d9ee98a1aeeb8b08318b29373cc7c95
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-16 14:38:21 +0900

    [level 2] Title: 튜플, Time: 513.31 ms, Memory: 40.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,5 +1,12 @@
 function solution(s) {
-    return arr = s.slice(1,-1).split("},{").map((el)=>el.replace(/{|}/g,'').split(",").map((el)=>+el)).sort((a,b)=>a.length-b.length).reduce((acc,cur)=>[...acc,...cur.filter((el)=>!acc.includes(el))],[]);
+    let arr = s.slice(2,-2).split("},{").map((el)=>el.split(',')).sort((a,b)=>a.length-b.length)
+    let answer = [+arr[0][0]];
+    for(let i=0;i<arr.length-1;i++){
+        answer.push(+arr[i+1].filter((el)=>!answer.includes(+el)))
+    }
+    return answer
+    // return arr = s.slice(1,-1).split("},{").map((el)=>el.replace(/{|}/g,'').split(",").map((el)=>+el)).sort((a,b)=>a.length-b.length).reduce((acc,cur)=>[...acc,...cur.filter((el)=>!acc.includes(el))],[]);
+    
     // let answer = [+arr[0]]
     // for(let i=1;i<arr.length;i++){
     //     let tmpArr = [...arr[i-1],...arr[i]];
