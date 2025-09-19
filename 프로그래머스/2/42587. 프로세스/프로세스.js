d9455634305d41bd111401df367d45fc27d78114
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-19 16:19:29 +0900

    [level 2] Title: 프로세스, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,21 +1,34 @@
 function solution(priorities, location) {
-    const arr =  priorities.map((el,idx)=>[el,idx]);
-    const answerIdx = arr[location];
-    let cnt =1;
-    while(priorities.length>0){
-        let tmp = priorities.shift();
-        let hasMax = priorities.some((el)=>el>tmp);
-        if(hasMax){
-            priorities.push(tmp);
-            tmp = arr.shift();
-            arr.push(tmp);
-        }
-        else{
-            if(arr[0][1]===location) return cnt;
-            else{
-                arr.splice(0,1);
-                cnt++;
-            }
-        }
+    const idxArr = priorities.map((el,idx)=>[el,idx])
+    const answer = idxArr[location];
+    const answerArr = [];
+    while(idxArr.length>0){
+        const cur = idxArr.shift();
+        const hasPriority = idxArr.some((el)=>el[0]>cur[0]);
+        if(hasPriority) idxArr.push(cur)
+        else answerArr.push(cur)
     }
+    return answerArr.indexOf(answer)+1
+    // console.log(answerArr.indexOf(priorities[location]))
+   
+    
+    // const arr =  priorities.map((el,idx)=>[el,idx]);
+    // const answerIdx = arr[location];
+    // let cnt =1;
+    // while(priorities.length>0){
+    //     let tmp = priorities.shift();
+    //     let hasMax = priorities.some((el)=>el>tmp);
+    //     if(hasMax){
+    //         priorities.push(tmp);
+    //         tmp = arr.shift();
+    //         arr.push(tmp);
+    //     }
+    //     else{
+    //         if(arr[0][1]===location) return cnt;
+    //         else{
+    //             arr.splice(0,1);
+    //             cnt++;
+    //         }
+    //     }
+    // }
 }
