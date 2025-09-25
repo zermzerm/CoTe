bd5b2d1e91a1ea222925e55e67b4e58ee2e21460
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-25 15:15:15 +0900

    [level 1] Title: 소수 만들기, Time: 11.03 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,26 +1,49 @@
 function solution(nums) {
-    let count = 0;
-
-    function isPrime(num) {
-        if (num < 2) return false;
-        for (let i = 2; i * i <= num; i++) {
-            if (num % i === 0) return false;
+    let cnt = 0;
+    
+    function isPrime(num){
+        for(let i=2;i<=num**0.5;i++){
+            if(num%i===0) return false
         }
         return true;
     }
-
-    function combine(start, depth, sum) {
-        if (depth === 3) {
-            if (isPrime(sum)) count++;
+    
+    function combination(arr,idx){
+        if(arr.length===3){
+            let sum = arr.reduce((acc,cur)=>acc+cur,0);
+            if(isPrime(sum)) cnt++;
             return;
         }
-        for (let i = start; i < nums.length; i++) {
-            combine(i + 1, depth + 1, sum + nums[i]);
+        for(let i=idx;i<nums.length;i++){
+            arr.push(nums[i])
+            combination(arr,i+1);
+            arr.pop();
         }
     }
+    combination([],0);
+    return cnt;
+//     let count = 0;
 
-    combine(0, 0, 0);
-    return count;
+//     function isPrime(num) {
+//         if (num < 2) return false;
+//         for (let i = 2; i * i <= num; i++) {
+//             if (num % i === 0) return false;
+//         }
+//         return true;
+//     }
+
+//     function combine(start, depth, sum) {
+//         if (depth === 3) {
+//             if (isPrime(sum)) count++;
+//             return;
+//         }
+//         for (let i = start; i < nums.length; i++) {
+//             combine(i + 1, depth + 1, sum + nums[i]);
+//         }
+//     }
+
+//     combine(0, 0, 0);
+//     return count;
     
     // return combination(nums, 3);
 }
