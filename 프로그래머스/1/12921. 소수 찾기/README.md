2efe7ef15ee3de5d777416b01e60ae9aa7e9c5da
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-09-25 15:37:13 +0900

    [level 1] Title: 소수 찾기, Time: 362.04 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/README.md
+++ 프로그래머스/1/12921. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 찾기 - 12921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921#) 
 
 ### 성능 요약
 
-메모리: 45.5 MB, 시간: 51.15 ms
+메모리: 61.8 MB, 시간: 362.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 18일 17:29:29
+2025년 09월 25일 15:37:11
 
 ### 문제 설명
 
--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,12 +1,42 @@
 function solution(n) {
-  let arr = new Array(n+1).fill(true);
-  arr[0] = arr[1] = false;
-  let answer = [];
-  for (let i = 2; i <= n; i++) {
-    if (arr[i]) answer.push(i);
-    for (let j = i * 2; j <= n; j += i) {
-      arr[j] = false;
+    const s = new Set();
+    for(let i=1; i<=n; i+=2){
+        s.add(i);
     }
-  }
-  return answer.length;
+    s.delete(1);
+    s.add(2);
+    for(let j=3; j<n; j++){
+        if(s.has(j)){
+             for(let k=j*2; k<=n; k+=j){    
+                s.delete(k);
+             }
+        }
+    }
+    return s.size;
 }
+
+// function solution(n) {
+//     let cnt = 0;
+//     function isPrime(num){
+//         for(let i=2;i<=num**0.5;i++){
+//             if(num%i===0) return false
+//         }
+//         return true;
+//     }
+//     for(let i=2;i<=n;i++){
+//         if(isPrime(i)) cnt++;
+//     }
+//     return cnt;
+    
+    
+//   // let arr = new Array(n+1).fill(true);
+//   // arr[0] = arr[1] = false;
+//   // let answer = [];
+//   // for (let i = 2; i <= n; i++) {
+//   //   if (arr[i]) answer.push(i);
+//   //   for (let j = i * 2; j <= n; j += i) {
+//   //     arr[j] = false;
+//   //   }
+//   // }
+//   // return answer.length;
+// }
