db51d929071053230c7e6ff4dfbd0182d6ec121b
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-10-02 13:20:04 +0900

    [level 1] Title: 옹알이 (2), Time: 0.62 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,22 +1,50 @@
 function solution(babbling) {
-    const arr = ["aya","ye","woo","ma"];
-    const regex = new RegExp(arr.join('|'), 'g');
-    const double = arr.map((el)=>el+el);
-    const regexDouble = new RegExp(double.join('|'), 'g');
-    const reBabbling = babbling.filter((el)=>el.match(regexDouble)===null);
-    return reBabbling.map((el)=>el.split(regex).filter(Boolean)).filter((el)=>el.length===0).length
-    // console.log(answer);
-    // for(let i=0;i<reBabbling.length;i++){
-    //     console.log(reBabbling[i].match(regex))
-    // }
-
-    // while(true){
-    //     for(let i=0;i<babbling.length;i++){
-    //         for(let j=0;)
+    const aya = ["aya","ye", "woo", "ma"];
+    let cnt = 0;
+    for(let i=0;i<babbling.length;i++){
+        let str = "";
+        let tmpAya = [...aya]
+        for(let j=0;j<babbling[i].length;j++){
+            str+=babbling[i][j];
+            if(tmpAya.indexOf(str)!==-1){
+                tmpAya = [...aya].filter((el)=>el!==str);
+                str="";
+                babbling[i] = babbling[i].slice(j+1);
+                j=-1;
+                if(babbling[i]==""){
+                    cnt++;
+                    continue;
+                }
+            }
+        }
+    }
+    return cnt;
+    // for(let i=0;i<babbling.length;i++){
+    //     let tmpAya = [...aya];
+    //     for(let j=0;j<tmpAya.length;j++){
+    //         if(babbling[i].includes(tmpAya[j])){
+    //             console.log(babbling[i], tmpAya[j],tmpAya)
+    //             babbling[i] = babbling[i].replace(tmpAya[j],"");
+    //             tmpAya = [...aya].filter((el)=>el!==tmpAya[j]);
+    //             j=-1;
+    //             console.log(babbling[i], tmpAya)
+    //             if(babbling[i]===""){
+    //             cnt++;
+    //             continue;
+    //         }
+    //         }
     //     }
-    // }
+    
+    
+    
+    
+    
+    
+    
+    // const arr = ["aya","ye","woo","ma"];
+    // const regex = new RegExp(arr.join('|'), 'g');
+    // const double = arr.map((el)=>el+el);
+    // const regexDouble = new RegExp(double.join('|'), 'g');
+    // const reBabbling = babbling.filter((el)=>el.match(regexDouble)===null);
+    // return reBabbling.map((el)=>el.split(regex).filter(Boolean)).filter((el)=>el.length===0).length
 }
-
-//double -> [ 'ayaaya', 'yeye', 'woowoo', 'mama' ]
-
-//babbling -> el = ayaye
