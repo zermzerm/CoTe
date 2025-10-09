efba444848289fe9acb9cb8e6f0b380d164d8148
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-10-09 16:44:00 +0900

    [level 3] Title: 네트워크, Time: 0.36 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,25 +1,37 @@
 function solution(n, computers) {
-//     const visited = Array(n).fill(false);
-//     const cnt = 0;    
-//     for(let i=0;i<n;i++){
-        
-//     }
+    const visited = Array(n).fill(false);
+    let cnt = 0;    
+    function dfs(node){
+        visited[node] = true;
+        for(let i=0;i<n;i++){
+            if(!visited[i] && computers[node][i]===1){
+                dfs(i);
+            }
+        }
+    }
+    for(let i=0;i<n;i++){
+        if(!visited[i]){
+            dfs(i);
+            cnt++;
+        }
+    }
+    return cnt;
     
-  let visited = new Array(n).fill(false);
-  let count = 0;
-  function dfs(node) {
-    visited[node] = true;
-    for (let i = 0; i < n; i++) {
-      if (computers[node][i] === 1 && !visited[i]) {
-        dfs(i);
-      }
-    }
-  }
-  for (let i = 0; i < n; i++) {
-    if (!visited[i]) {
-      dfs(i);
-      count++;
-    }
-  }
-  return count;
+  // let visited = new Array(n).fill(false);
+  // let count = 0;
+  // function dfs(node) {
+  //   visited[node] = true;
+  //   for (let i = 0; i < n; i++) {
+  //     if (!visited[i] && computers[node][i] === 1) {
+  //       dfs(i);
+  //     }
+  //   }
+  // }
+  // for (let i = 0; i < n; i++) {
+  //   if (!visited[i]) {
+  //     dfs(i);
+  //     count++;
+  //   }
+  // }
+  // return count;
 }
