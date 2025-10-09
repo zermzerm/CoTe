736275b4ff5891e8614d8ee003deef6fd70d0114
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-10-09 15:54:42 +0900

    [level 3] Title: 정수 삼각형, Time: 4.44 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,20 +1,43 @@
-function solution(triangle) {
-    return Math.max(...triangle.reduce((dp, raw) => {
-            return raw.map((v, i) => {
-              return Math.max((i === 0 ? 0 : dp[i - 1]), i === dp.length ? 0 : dp[i]) + v; 
-            });
-        }, [0]));
-}
-
 // function solution(triangle) {
 //     for(let i=triangle.length-2;i>=0;i--){
 //         for(let j=0;j<triangle[i].length;j++){
-//             triangle[i][j] += Math.max(triangle[i+1][j],triangle[i+1][j+1])
+//             triangle[i][j]+=Math.max(triangle[i+1][j],triangle[i+1][j+1]);
 //         }
 //     }
-    
 //     return triangle[0][0]
+    
+    
+    // let answer =[];
+    // function dp(depth,idx,sum){
+    //     if(depth===triangle.length){
+    //         answer.push(sum);
+    //         return;
+    //     }
+    //     sum+=triangle[depth][idx]
+    //     dp(depth+1,idx,sum)
+    //     dp(depth+1,idx+1,sum)
+    // }
+    // dp(0,0,0);
+    // return Math.max(...answer)
 // }
+    
+    // function solution(triangle) {
+    // return Math.max(...triangle.reduce((dp, raw) => {
+    //         return raw.map((v, i) => {
+    //           return Math.max((i === 0 ? 0 : dp[i - 1]), i === dp.length ? 0 : dp[i]) + v; 
+    //         });
+    //     }, [0]));
+// }
+
+function solution(triangle) {
+    for(let i=triangle.length-2;i>=0;i--){
+        for(let j=0;j<triangle[i].length;j++){
+            triangle[i][j] += Math.max(triangle[i+1][j],triangle[i+1][j+1])
+        }
+    }
+    
+    return triangle[0][0]
+}
 
 // function solution(triangle) {
 //     const answer =[];
