90dde0e26a2ea40f5a7354702cb34b2585d97a66
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-10-09 14:15:12 +0900

    [level 1] Title: 소수 찾기, Time: 234.39 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,17 +1,18 @@
 function solution(n) {
-    const s = new Set();
-    s.add(2);
+    const arr = new Set();
+    arr.add(2);
     for(let i=3;i<=n;i+=2){
-        s.add(i);
+        arr.add(i)
     }
-    for(let i=3;i<=n;i++){
-        if(s.has(i)){
+    for(let i=3;i<=n;i+=2){
+        if(arr.has(i)){
             for(let j=i*2;j<=n;j+=i){
-                s.delete(j)
+                arr.delete(j);
             }
         }
     }
-    return s.size
+    return arr.size;
+}
     
     // const s = new Set();
     // for(let i=1; i<=n; i+=2){
@@ -27,7 +28,7 @@ function solution(n) {
     //     }
     // }
     // return s.size;
-}
+// }
 
 // function solution(n) {
 //     let cnt = 0;
