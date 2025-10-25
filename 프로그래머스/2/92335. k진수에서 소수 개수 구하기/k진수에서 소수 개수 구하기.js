d75a42e3f301c1b50af78792468a5286b146900f
kyeong.seo <91651812+zermzerm@users.noreply.github.com>
2025-10-25 11:50:16 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 10.22 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,17 +1,19 @@
 function solution(n, k) {
-    let newNum = ""+n.toString(k);
-    let tmp = '';
+    let newNum = n.toString(k).split(0).filter((el)=>el!==""&&el!=="1");
     let answer = 0;
     for(let i=0;i<newNum.length;i++){
-        if(newNum[i]!=="0"){
-            tmp+=newNum[i];
-        }
-        else if(newNum[i]==="0"&&tmp!==""){
-            if(isPrime(+tmp)) answer++;
-            tmp = "";
-        }
+        if(isPrime(+newNum[i])) answer++;
     }
-    if(tmp!==""&&isPrime(+tmp)) answer++;
+    // for(let i=0;i<newNum.length;i++){
+    //     if(newNum[i]!=="0"){
+    //         tmp+=newNum[i];
+    //     }
+    //     else if(newNum[i]==="0"&&tmp!==""){
+    //         if(isPrime(+tmp)) answer++;
+    //         tmp = "";
+    //     }
+    // }
+    // if(tmp!==""&&isPrime(+tmp)) answer++;
     return answer
     
     // let answer =0;
@@ -20,7 +22,6 @@ function solution(n, k) {
 }
 
 function isPrime(n){
-    if(n===1) return false;
     for(let i=2;i<=n**0.5;i++){
         if(n%i===0) return false;
     }
